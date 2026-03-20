cf136dec01a782a2ba9e99ede9bfd31a2fea5cc2
Abhirajpatel8789 <122907724+Abhirajpatel8789@users.noreply.github.com>
2026-03-20 15:14:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let container_box = document.querySelector(".container");
+        
+        
+        for(let i = 1; i <=800; i++){
+
+            let div = document.createElement("div");
+            div.className = "square";
+            container_box.append(div);
+            
+            let randomColor  =()=> Math.floor(Math.random()*256);
+            
+            div.addEventListener("mouseover",()=>{
+                div.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`
+
+                
+            })
+            div.addEventListener("mouseout",()=>{
+                
+                div.style.transition="1s ease" ;
+                    div.style.backgroundColor = ""
+                
+                })
+
+
+        }
